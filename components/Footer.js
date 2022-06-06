f24c95e8e2b9ab39e632a78f8510562183253575
Kunwar Aditya <adityakunwar0147@gmail.com>
2022-06-06 07:04:26 +0530

    init

--- components/Footer.js
+++ components/Footer.js
@@ -30,9 +30,8 @@ const Footer = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121059.04360433813!2d73.79292685256993!3d18.52460355342981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf2e67461101%3A0x828d43bf9d9ee343!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1654476247217!5m2!1sen!2sin"
             width="400"
             height="300"
-            allowfullscreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
             className="mx-auto  my-5 rounded-md  md:flex-[0.3] lg:flex-[0.7] "
           ></iframe>
         </div>
